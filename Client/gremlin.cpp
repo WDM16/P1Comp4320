c7c1a307509acd540a61f70ff8fd776af0fe6181
JShaw282 <jackson@shaw34.com>
2022-10-18 21:44:28 -0500

    sup

--- Client/gremlin.cpp
+++ Client/gremlin.cpp
@@ -4,25 +4,34 @@
 #include <random>
 using namespace std;
 
-bool gremlin() {
 
-    int damagedPacket;
+bool gremlin(int randNum) {
+
+    int damagedPacket = randNum;
     return true;
     
 
 }
 
-//Jackson - Generate random number between 1 and 10
-int main(){
-
-	// Providing a seed value
+int generateRandomNum() {
+    // Providing a seed value
 	srand((unsigned) time(NULL));
 
 	// Get a random number
 	int random = 1+ (rand() % 10);
 
 	// Print the random number
-	cout<<random<<endl;
+	//cout<<random<<endl;
+    //According to project description we will pass this number to the gremlin function and can divide by 10
+    //as needed if we want to follow the description
+    return random;
+}
 
+//Jackson - Generate random number between 1 and 10
+int main(){
+    int a = generateRandomNum();
+	cout << gremlin(a);
+    
 	return 1;
 }
+
