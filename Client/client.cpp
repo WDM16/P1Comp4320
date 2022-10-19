cdfb173eaae7ba6c436ba4a1c7b5562da0a43c59
WDM16 <will.may27@gmail.com>
2022-10-19 12:50:17 -0500

    Update client.cpp

--- Client/client.cpp
+++ Client/client.cpp
@@ -17,11 +17,11 @@ int main(int argc, char **argv) {
     sd = socket(AF_INET, SOCK_DGRAM, 0);
 
     bzero(&server, sizeof(server)); //Covered in lecture, said we need this
-    
+
     server.sin_family = AF_INET;
     server.sin_port = htons(12345); //Port number
     hp = gethostbyname("10.0.0.202"); //Server IP address
-    bcopy(hp->h_addr, &(server.sin_addr), hp->h_length);
+    bcopy(hp->h_addr, &(server.sin_addr), hp->h_length); //Binds IP address and Port number?
 
     for(;;) {
         //Sends messgae to server
