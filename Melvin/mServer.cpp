c53f1b5ec8be408b74277d6f7f2170622a693172
WDM16 <will.may27@gmail.com>
2022-11-01 15:37:23 -0500

    Update mServer.cpp

--- Melvin/mServer.cpp
+++ Melvin/mServer.cpp
@@ -150,18 +150,3 @@ int main(int argc, char *argv[]) {
     cout << "Successfully closed the socket..." << endl;
     return 0;
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-COMP4320/UDPServer.cpp at main · MelvinM8/COMP4320
