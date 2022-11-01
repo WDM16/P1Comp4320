3dd904ee71e5f44eb08d454c6ce3e5fbd335bc7b
JShaw282 <jackson@shaw34.com>
2022-11-01 16:30:42 -0500

    n

---
+++ BareMinFreshStart/client0.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <strings.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char **argv) {
+    int sd;
+    struct sockaddr_in server;
+    struct hostent *hp;
+
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    bzero(&server, sizeof(server)); //Covered in lecture, said we need this
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(12345); //Port number
+    hp = gethostbyname("10.0.0.202"); //Server IP address
+    bcopy(hp->h_addr, &(server.sin_addr), hp->h_length); //Binds IP address and Port number? It copys from location one to location two
+    for(;;) {
+        //Sends messgae to server
+        sendto(sd, "HI", 2, 0, (struct sockaddr *) &server, sizeof(server));
+        sleep(5);
+
+    }
+    close(sd);
+
+}
