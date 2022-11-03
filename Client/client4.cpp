ccd0933547c92ab26495deb1190048ee2f119e2b
JShaw282 <jackson@shaw34.com>
2022-11-03 12:34:22 -0500

    Update client4.cpp

--- Client/client4.cpp
+++ Client/client4.cpp
@@ -143,7 +143,9 @@ void createPackets() {
 		}
 		setChecksum(packet);
 		gremlin(packet);
-		sequenceNumber = (sequenceNumber == '0') ? '1' : '0';
+		if(sequenceNumber == '0')
+		sequenceNumber = '1';
+		else sequenceNumber == '0'; 
 		if (packet[1] == 'Y') { //Checks if packet is not lost
 			std::string packetString = "";
 			for (int i = 0; i < 48; i++) {
