8db279af47063091d11ca3377d8fef1a9f256205
WDM16 <will.may27@gmail.com>
2022-11-03 12:50:21 -0500

    Update server4.cpp

--- Server/server4.cpp
+++ Server/server4.cpp
@@ -65,7 +65,7 @@ bool validateChecksum(char buffer[]) {
 //Method that writes the buffer body to filestream
 int writeFile(ofstream &file, char buffer[]) {
 	for (int i = 7; i < PACKET_SIZE; i++) {
-		if (buffer[i] != '\0')
+		if (buffer[i] != '\0') 
 			file << buffer[i];
 	}
 	return 0;
@@ -79,91 +79,63 @@ int sendResponse(char resp[], socklen_t socketLength) {
 	return 0;
 }
 
-/**
- * Listens for a message from the client and writes the output file.
- */
-int receiveMessage()
-{
+
+//Method that listens for message from client and writes the output
+int receiveMessage() {
 	char buffer[PACKET_SIZE] = {0};
 	ofstream file;
 	socklen_t socketLength;
 	int msgLength;
-
 	socketLength = sizeof(cliaddr);
 	file.open("OutputFile.txt");
-
-	// Checks if file opened properly
-	if (!file.is_open())
-	{
+	if (!file.is_open()) { //Checks if the file is not open
 		cout << "Failed to open output file." << endl;
 		exit(EXIT_FAILURE);
 	}
-
 	char previousSequenceNumber;
-	while (true)
-	{
-		msgLength = recvfrom(sockfd, (char *)buffer, PACKET_SIZE,
-							 MSG_WAITALL, (struct sockaddr *)&cliaddr,
-							 &socketLength);
+	while (true) {
+		msgLength = recvfrom(sockfd, (char *)buffer, PACKET_SIZE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &socketLength);
 		buffer[msgLength] = '\0';
 		char sequenceNumber = buffer[0];
-
-		if (sequenceNumber == '\0')
-		{
-			// This is the last packet. Break out to close the file stream.
+		if (sequenceNumber == '\0') {
 			break;
 		}
-		else if (sequenceNumber == previousSequenceNumber)
-		{
+		else if (sequenceNumber == previousSequenceNumber) {
 			cout << "Packet " << sequenceNumber << " has a duplicate sequence number. A packet was lost." << endl;
 		}
-		else
-		{
+		else {
 			cout << "Packet " << sequenceNumber << " received. Checking for errors..." << endl;
-			// checksum validation fails or the sequence number is invalid
-			if (!validateChecksum(buffer) || (sequenceNumber != '0' && sequenceNumber != '1'))
-			{
+
+			if (!validateChecksum(buffer) || (sequenceNumber != '0' && sequenceNumber != '1')) {
 				cout << "Packet " << sequenceNumber << " is damaged." << endl;
 			}
-			else
-			{
+			else {
 				cout << "Packet " << sequenceNumber << " is ok." << endl;
 			}
 		}
-
-		// update the previous number to detect future packet loss
 		previousSequenceNumber = sequenceNumber;
-
 		cout << "Printing the first 48 bytes of packet " << sequenceNumber << ":" << endl;
-		// print out the first 48 bytes of the body
-		for (int i = 0; i < 48; i++)
-		{
+		for (int i = 0; i < 48; i++) {
 			cout << buffer[i];
 		}
 		cout << endl;
-
 		writeFile(file, buffer);
-
 		char resp[18];
 		sprintf(resp, "Packet %c received.", sequenceNumber);
 		sendResponse(resp, socketLength);
 	}
 	cout << "Final packet received. Writing file." << endl;
 	file.close();
-
 	char successMsg[] = "PUT successfully completed";
 	sendResponse(successMsg, socketLength);
-
 	return 0;
 }
 
+//Main Method of program
 int main(int argc, char const *argv[])
 {
 	cout << "Starting server" << endl;
-
 	init();
-
 	receiveMessage();
-
 	return 0;
 }
